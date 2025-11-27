9ea4387e83b318b8624140d70153afcde17f757e
Muhammad Munir <hrcoder3@gmail.com>
2025-11-27 22:33:16 +0500

    nodemailer fails to connect here

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv');
+const cors=require("cors");
 const connectDB = require("./config/db.js");
 const userRoutes = require('./routes/user.routes.js');
 const app = express()
 dotenv.config();
 // Connect Database
 connectDB();
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 app.get('/', (req, res) => {
   res.send('real estate assignment website introduction to management!')
 })
-app.use(express.json());
 // Use Routes
 app.use('/users', userRoutes);
 const PORT = process.env.PORT || 5000;
